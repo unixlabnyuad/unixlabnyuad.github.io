d01f4dd5dbd9a8ff1d9112b6846ebfc7dfee5985
Azza Abouzied <directorunixlabnyuad@gmail.com>
2019-08-27 15:34:33 +0400

    website update

---
+++ data/shifts.js
@@ -0,0 +1,32 @@
+var shifts = [
+    {
+        id: 0,
+        day: "monday",
+        from: "14:00",
+        to: "15:30"
+    },
+    {
+        id: 0,
+        day: "tuesday",
+        from: "12:00",
+        to: "20:30"
+    },
+    {
+        id: 1,
+        day: "monday",
+        from: "12:00",
+        to: "15:30"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "19:00",
+        to: "21:00"
+    },
+    {
+        id: 2,
+        day: "wednesday",
+        from: "19:00",
+        to: "21:30"
+    },
+];
---
+++ data/tutors.js
@@ -0,0 +1,45 @@
+var tutors =  [
+    {
+        id: 0,
+        name: "Alejandra Trejo Rodriguez",
+        bio: "Hi there! This is Alejandra but just call me Ale. I love art and technology so if I’m not in the Unix lab you can probably find me at the IM lab. Bring me your questions and I’ll try to answer them. If not, we can cry together. I’ve been told I’m a good listener!",
+        image: "images/tutors/ale-e1550660921516-225x300.jpg",
+        email: "alejandra.trejo@nyu.edu",
+    },
+    {
+        id: 1,
+        name: "Michael Fengyuan Liu",
+        bio: "Michael is a Junior majoring in Computer Science. He is fluent in English, Mandarin, C/C++ and Python. Apart from CS coursework, he had experience with Pandas, Django, and web scraping, as well as Matlab and R. He is passionate about solving algorithmic problems (although not particularly an expert at it). He also cares about basketball, meaning of life, and having dinner on time.",
+        image: "images/tutors/Michael_Liu-214x300.jpg",
+        email: "fl1092@nyu.edu",
+    },
+    {
+        id: 2,
+        name: "Jaisal Freidman",
+        bio: "Aspiring quant, inspiring C aficionado, recursively edge testing my life decisions. I can help you with slaving Arduinos, debugging C/C++, git’ing, launching your hot new (pythonic) website, simulating financial collapses in MATLAB or Jupyter, and finding hidden APIs. We can also geek out about the Black-Scholes-Equation, Levy fat tail distributions, or hedge funds bonuses if you’re into that.\n" +
+            "P.S.  I won’t write code for you, byte me.",
+        image: "images/tutors/DSC_0502_22-683x1024.jpg",
+        email: "jpf409@nyu.edu",
+    },
+    {
+        id: 3,
+        name: "Anh Mai",
+        bio: "CS and Math major. I can help you with Java, C++, Python, Matlab. I enjoy doing competitive programming as a hobby. I also would love to discuss any Math problems that you are interested.",
+        image: "images/tutors/anh.png",
+        email: "alm818@nyu.edu",
+    },
+    {
+        id: 4,
+        name: "Mingyang Wang",
+        bio: "Ming here. I can help you with C++, Python and web development (React, Redux, Node, etc.). You can also torture me with questions on Haskell and/or functional programming ",
+        image: "images/tutors/mingyang.jpeg",
+        email: "mw3165@nyu.edu",
+    },
+    {
+        id: 5,
+        name: "Jacinta Hu",
+        bio: "It took me a while to decide to be a CS major, but now I’m a lab monitor so I guess it’s officially official. Feel free to ask me anything and everything and if I can’t help, I’ll apologize sincerely and most likely direct you to the glorious cesspool that is the internet. If I’m not in the lab, you can probably find me tap dancing at Saadiyat Beach Club, setting on the volleyball court, or yelling at the grand piano in the Arts Centre.",
+        image: "images/tutors/jacinta-1-532x1024.jpeg",
+        email: "jh5372@nyu.edu",
+    },
+];
