cf271e0d74d1d477139239416b36e857aadbc84e
Manesha Ramesh <41325932+ManeshaRamesh@users.noreply.github.com>
2019-09-04 18:57:00 +0400

    Manesha - shifts

--- data/shifts.js
+++ data/shifts.js
@@ -12,6 +12,24 @@ var shifts = [
         to: "20:30"
     },
     {
+        id: 1,
+        day: "monday",
+        from: "12:00",
+        to: "15:30"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "19:00",
+        to: "21:00"
+    },
+    {
+        id: 2,
+        day: "monday",
+        from: "17:00",
+        to: "18:30"
+    },
+    {
         id: 2,
         day: "wednesday",
         from: "17:00",
@@ -90,21 +108,27 @@ var shifts = [
         to: "01:00"
     },
     {
-        id: 6,
+        id: 9,
+        day: "sunday",
+        from: "12:00",
+        to: "14:00"
+    }, 
+    {
+        id: 9,
+        day: "sunday",
+        from: "18:00",
+        to: "20:00"
+    }, 
+    {
+        id: 9,
         day: "monday",
-        from: "02:30",
-        to: "05:00"
-    },
+        from: "18:00",
+        to: "20:00"
+    }, 
     {
-        id: 6,
-        day: "wednesday",
-        from: "02:30",
-        to: "05:00"
-    },
-    {
-        id: 6,
-        day: "friday",
-        from: "11:00",
-        to: "01:00"
+        id: 9,
+        day: "thursday",
+        from: "10:00",
+        to: "12:00"
     }
 ];
