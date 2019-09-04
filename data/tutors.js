e078df57591a505cf2f42d1b3c0c05d4d4141c35
Manesha Ramesh <41325932+ManeshaRamesh@users.noreply.github.com>
2019-09-04 18:48:45 +0400

    Manesha Bio

--- data/tutors.js
+++ data/tutors.js
@@ -62,5 +62,12 @@ var tutors =  [
         bio: "As someone who wrote his first line of 'Hello World!' here in college, I can understand the struggle and frustration while writing code. Feel free to come talk to me about anything CS/tech related, or if you're in the mood for lame jokes. Otherwise, I can be found taking photos at events and stuffing myself with free food wherever I find it. You'll recognize me from my big curly hair.",
         image: "images/tutors/IMG-7663.JPG",
         email: "as10505@nyu.edu"
+    },
+    {
+        id: 9,
+        name: "Manesha Ramesh",
+        bio: "Hi! I am Manesha and I am here to help you with C, C++, python and web development (NodeJS, ReactJS, HTML, CSS). Feel free to bombard me with questions and I'll do my best to help you! You can find me in the UNIX lab or you can find me around campus reading comic books!",
+        image: "images/tutors/jacinta-1-532x1024.JPG",
+        email: "mr4684@nyu.edu"
     }
 ];
