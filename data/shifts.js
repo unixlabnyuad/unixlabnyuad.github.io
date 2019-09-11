8cb493eb86b2a832ce487f272c5491548d21f70c
Jaisal Friedman <jaisal1024@gmail.com>
2019-09-11 22:22:46 +0400

    updated my shifts

--- data/shifts.js
+++ data/shifts.js
@@ -19,6 +19,12 @@ var shifts = [
     },
     {
         id: 2,
+        day: "monday",
+        from: "17:00",
+        to: "19:00"
+    },
+    {
+        id: 2,
         day: "wednesday",
         from: "17:00",
         to: "19:00"
@@ -27,7 +33,7 @@ var shifts = [
         id: 2,
         day: "thursday",
         from: "14:00",
-        to: "16:30"
+        to: "16:00"
     },
     {
         id: 7,
