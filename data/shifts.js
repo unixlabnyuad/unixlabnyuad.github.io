a8bbceeccf869eb27e54a891ca73dbe7b514a20c
Maahir Ur Rahman <msmaahirur@gmail.com>
2020-02-08 17:19:43 +0400

    Update shifts.js

--- data/shifts.js
+++ data/shifts.js
@@ -5,4 +5,16 @@ var shifts = [
         from: "15:00",
         to: "19:00"
     },
+    {
+        id: 6,
+        day: "monday",
+        from: "12:30",
+        to: "16:00"
+    },
+    {
+        id: 6,
+        day: "wednesday",
+        from: "12:30",
+        to: "16:00"
+    }
 ];
