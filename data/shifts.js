823c63ef84bcd1ca7d72b6be45bd3c96d93344f2
Ali Shazal <as10505@nyu.edu>
2020-02-09 11:01:34 +0400

    Update shifts.js

--- data/shifts.js
+++ data/shifts.js
@@ -1,11 +1,5 @@
 var shifts = [
     {
-        id: 0,
-        day: "sunday",
-        from: "15:00",
-        to: "19:00"
-    },
-    {
         id: 5,
         day: "sunday",
         from: "10:00",
