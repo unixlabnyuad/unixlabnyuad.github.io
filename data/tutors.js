ba5fabfe0a8eb43c78cf35214ae8d8ead1f65eec
Maahir Ur Rahman <msmaahirur@gmail.com>
2020-03-06 17:04:35 +0400

    Zoom

--- data/tutors.js
+++ data/tutors.js
@@ -53,6 +53,7 @@ var tutors =  [
         bio: "Heya! I'm Maahir, a Computer Science and Mathematics senior who loves doing a little bit of everything. Aside from helping you with the normal CS course stuff, I've dabbled in Front-End(HTML,CCS,JS) and am learning Golang at the moment. I can help you with understanding mathematical motivations, *insert Big Oh*, and maybe even make this daunting process of learning CS a fun one.",
         image: "images/tutors/maahir-profile.jpg",
         email: "maahirur@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/7441434375",
     },
         {
         id: 7,
