0976bb65a01145289eebd174a3b93d6197b946da
Wahib Kamran <wahibkamran@nyu.edu>
2020-09-17 19:08:37 +0400

    updated shift.js

--- data/shifts.js
+++ data/shifts.js
@@ -5,5 +5,41 @@ var shifts = [
         from: "10:30",
         to: "11:30"
     },
+    {
+        id: 1,
+        day: "sunday",
+        from: "10:00",
+        to: "11:00"
+    },
+    {
+        id: 1,
+        day: "sunday",
+        from: "14:00",
+        to: "15:00"
+    },
+    {
+        id: 1,
+        day: "monday",
+        from: "10:00",
+        to: "12:00"
+    },
+    {
+        id: 1,
+        day: "tuesday",
+        from: "10:00",
+        to: "12:00"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "10:00",
+        to: "12:00"
+    },
+    {
+        id: 1,
+        day: "thursday",
+        from: "10:00",
+        to: "12:00"
+    },
 ];
 
