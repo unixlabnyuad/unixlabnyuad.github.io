d0398c47f50cb28081e75d27825c0dc150f3bbec
TofunmiKupoluyi <tofunmi@live.com>
2020-11-10 17:52:33 +0400

    Update tutors.js

--- data/tutors.js
+++ data/tutors.js
@@ -2,7 +2,7 @@ var tutors =  [
     {
         id: 0,
         name: "Tofunmi Kupoluyi",
-        bio: "Hey! I am a man of many names: T, TK, Tofu, Tofunmi. Feel free to call me any. I can help with C, C++, Python, Java, Web Dev (NodeJS, React, HTML, CSS), SQL and MongoDB. Double-majoring in Math so I could also try to work through some proofs with you if you're stuck with Discrete. Outside the academic realm, I love working and talking about startup projects/ideas. So, if you have an interesting idea you want to run by me, come through!",
+        bio: "Hey! I am: T, TK, Tofu, Tofunmi. Feel free to call me any. I can help with C, C++, Python, Java, Web Dev (NodeJS, React, HTML, CSS), SQL and MongoDB. Double-majoring in Math so I could also try to work through some proofs with you if you're stuck with Discrete. Outside the academic realm, I love working and talking about startup projects/ideas. So, if you have an interesting idea you want to run by me, come through!",
         image: "images/tutors/IMG_3140.JPG",
         email: "jdk461@nyu.edu",
         zoom: "https://nyu.zoom.us/j/3758494458"
