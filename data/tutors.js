beb2cfaf294ac6c4a031a54a53d09eb2747fceb5
NYUAD <nyuad@C2-LIB-MBP-20.local>
2021-01-21 00:28:51 +0400

    new profile

--- data/tutors.js
+++ data/tutors.js
@@ -10,8 +10,8 @@ var tutors =  [
     {
         id: 1,
         name: "Wajahat Mirza",
-        bio: "Hi all, I am Wajahat. You can call me Waji if that's easier! I am here to help you with any questions with regards to programming, machine learning, and full-stack development. You can always reach out to me for help with Python, C/C++, ML libraries, or even your resume. If you wanna chat about FinTech, anthropology, or formula 1, don't hesitate to contact me, I can bore you all day long",
-        image: "images/tutors/wajahat.jpg",
+        bio: "Hi all, I am Wajahat. You can call me Waji if that's easier! I am here to help you with any questions with regards to programming, machine learning, and full-stack development. You can always reach out to me for help with Python, C/C++, ML libraries, or even your resume. If you wanna chat about FinTech, anthropology, or formula 1, don't hesitate to contact me, I can bore you all day long!",
+        image: "images/tutors/waji.jpg",
         email: "wajahat.mirza@nyu.edu",
         zoom: "https://nyu.zoom.us/j/6458810058?pwd=cUtueDl0bUNqSUIvS0lQeGJVNVlSUT09",
     },
