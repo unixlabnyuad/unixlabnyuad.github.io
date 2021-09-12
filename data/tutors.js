b258fd9d0fa9fa82bafbc3bb9d14c25877096241
azzadev <azza.abouzied@gmail.com>
2021-09-12 14:02:29 +0400

    Update unix lab schedule/tutors for new batch.

--- data/tutors.js
+++ data/tutors.js
@@ -1,11 +1,11 @@
 var tutors =  [
 /*    {
         id: 1,
-        name: "Wajahat Mirza",
-        bio: "Hi all, I am Wajahat. You can call me Waji if that's easier! I am here to help you with any questions with regards to programming, machine learning, and full-stack development. You can always reach out to me for help with Python, C/C++, ML libraries, or even your resume. If you wanna chat about FinTech, Anthropology, or Formula 1, don't hesitate to contact me, I can bore you all day long!",
+        name: "Aw Samtut Or",
+        bio: "About me ... I'm happy to help you with ...",
         image: "images/tutors/waji.jpg",
-        email: "wajahat.mirza@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/6458810058?pwd=cUtueDl0bUNqSUIvS0lQeGJVNVlSUT09",
+        email: "@nyu.edu",
+        zoom: "",
     },
 */
 ];
