8816812a93e0536fea546a0c294cfb437da57262
Akhat <39861196+akhatsuleimenov@users.noreply.github.com>
2021-09-12 18:39:57 +0400

    Akhat Suleimenov added as tutor

--- data/tutors.js
+++ data/tutors.js
@@ -1,4 +1,14 @@
 var tutors =  [
+   {
+        id: 1,
+        name: "Akhat Suleimenov",
+        bio: "I am 21 years old. Sophmore at NYU Abu Dhabi. Alumni of 42 Software Engineering School in Silicon Valley. I'm happy to help you with CSO, SE, Intro to CS, Algorithms, Data Structures, Discrete Math, and any Math course.",
+        image: "images/tutors/Akhat.jpg",
+        email: "akhat.suleimenov@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/6171613743?pwd=Y0lSakR5Z3pUSjB1LzJnZkcrMWJIQT09",
+    },
+
+
 /*    {
         id: 1,
         name: "Aw Samtut Or",
