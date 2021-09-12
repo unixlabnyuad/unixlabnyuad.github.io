5f7d4a932d5f83c3a58826038e965a99f2c7b8f1
dweggy <colacolaojm@hotmail.com>
2021-09-12 23:26:21 +0400

    update shifts for everyone else

--- data/shifts.js
+++ data/shifts.js
@@ -41,4 +41,118 @@ var shifts = [
         from: "14:00",
         to: "18:00"
     },
+    {
+        id: 2,
+        day: "sunday",
+        from: "20:30",
+        to: "22:30"
+    },
+    {
+        id: 2,
+        day: "monday",
+        from: "20:30",
+        to: "22:30"
+    },
+    {
+        id: 2,
+        day: "tuesday",
+        from: "16:00",
+        to: "18:00"
+    },
+    {
+        id: 2,
+        day: "wednesday",
+        from: "20:30",
+        to: "22:30"
+    },
+    {
+        id: 2,
+        day: "thursday",
+        from: "16:00",
+        to: "18:00"
+    },
+    {
+        id: 3,
+        day: "tuesday",
+        from: "20:30",
+        to: "23:30"
+    },
+    {
+        id: 3,
+        day: "thursday",
+        from: "11:00",
+        to: "12:00"
+    },
+    {
+        id: 3,
+        day: "thursday",
+        from: "15:00",
+        to: "16:00"
+    },
+    {
+        id: 4,
+        day: "thursday",
+        from: "10:00",
+        to: "11:30"
+    },
+    {
+        id: 4,
+        day: "saturday",
+        from: "21:00",
+        to: "25:30"
+    },
+    {
+        id: 5,
+        day: "monday",
+        from: "20:00",
+        to: "24:00"
+    },
+    {
+        id: 5,
+        day: "wednesday",
+        from: "22:00",
+        to: "24:30"
+    },
+    {
+        id: 6,
+        day: "sunday",
+        from: "15:00",
+        to: "16:30"
+    },
+    {
+        id: 6,
+        day: "monday",
+        from: "10:00",
+        to: "12:30"
+    },
+    {
+        id: 6,
+        day: "tuesday",
+        from: "15:00",
+        to: "16:30"
+    },
+    {
+        id: 6,
+        day: "wednesday",
+        from: "10:00",
+        to: "12:30"
+    },
+    {
+        id: 6,
+        day: "thursday",
+        from: "16:00",
+        to: "18:00"
+    },
+    {
+        id: 6,
+        day: "thursday",
+        from: "23:00",
+        to: "25:30"
+    },
+    {
+        id: 6,
+        day: "friday",
+        from: "23:00",
+        to: "25:30"
+    },
 ];
