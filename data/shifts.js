a9a76e02e8dcf49403ccdcc913a2098640b99ba5
dweggy <colacolaojm@hotmail.com>
2021-09-12 23:11:14 +0400

    update shifts file for Akhat ( to test if it works )

--- data/shifts.js
+++ data/shifts.js
@@ -1,8 +1,44 @@
 var shifts = [
-    /*{
-        id: 3,
+    {
+        id: 1,
+        day: "monday",
+        from: "11:00",
+        to: "12:00"
+    },
+    {
+        id: 1,
+        day: "monday",
+        from: "16:30",
+        to: "19:00"
+    },
+    {
+        id: 1,
+        day: "tuesday",
+        from: "16:30",
+        to: "19:00"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "19:30",
+        to: "21:30"
+    },
+    {
+        id: 1,
+        day: "thursday",
+        from: "19:30",
+        to: "21:30"
+    },
+    {
+        id: 1,
+        day: "friday",
+        from: "11:00",
+        to: "12:00"
+    },
+    {
+        id: 1,
         day: "friday",
         from: "14:00",
-        to: "19:00"
-    },*/
+        to: "18:00"
+    },
 ];
