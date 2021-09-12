4a71ff56e5a0667e01f4f9f2ab7aa6011fc3dd32
dweggy <colacolaojm@hotmail.com>
2021-09-12 23:10:17 +0400

    update tutors file with new tutors

--- data/tutors.js
+++ data/tutors.js
@@ -1,11 +1,51 @@
 var tutors =  [
-   {
+    {
         id: 1,
         name: "Akhat Suleimenov",
-        bio: "I am 21 years old. Sophmore at NYU Abu Dhabi. Alumni of 42 Software Engineering School in Silicon Valley. I'm happy to help you with CSO, SE, Intro to CS, Algorithms, Data Structures, Discrete Math, and any Math course.",
+        bio: "I am 21 years old. Sophomore at NYU Abu Dhabi. Alumni of 42 Software Engineering School in Silicon Valley. I'm happy to help you with CSO, SE, Intro to CS, Algorithms, Data Structures, Discrete Math, and any Math course.",
         image: "images/tutors/Akhat.jpg",
         email: "akhat.suleimenov@nyu.edu",
         zoom: "https://nyu.zoom.us/j/6171613743?pwd=Y0lSakR5Z3pUSjB1LzJnZkcrMWJIQT09",
+    },
+    {
+        id: 2,
+        name: "Jun Ooi",
+        bio: "I'm a sophomore majoring in Computer Science and minoring in Interactive Media. Feel free to talk to me about front-end Web Development ( NodeJS, ReactJS, HTML, CSS ) or just a chat in general if you want to discuss anything but have no idea where to start! I'm also an avid fan of tea, but you'll find me with a cup of coffee most of the time.",
+        image: "images/tutors/JunOoi.jpg",
+        email: "jmo460@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/97876954385",
+    },
+    {
+        id: 3,
+        name: "Nati Tsegaye",
+        bio: "Hi, I’m a Junior majoring in EE and minoring in CS. I can help with C++, Python and JavaScript. In addition, if you are interested in web or mobile dev and need help with the MERN stack or Flutter find me in UNIX lab and we can figure things out together.",
+        image: "images/tutors/Nati_Tsegaye.jpg",
+        email: "nht249@nyu.edu",
+        zoom: "",
+    },
+    {
+        id: 4,
+        name: "Khola Jamshad",
+        bio: "Hi, I'm a senior majoring in Math and minoring in Computer Science. I would love to help you develop your skills in python and get a better understanding of the mathematical side of CS!",
+        image: "images/tutors/Khola_RAintro.jpg",
+        email: "kj1263@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/97401480668",
+    },
+    {
+        id: 5,
+        name: "Lachlan Pham",
+        bio: "I'm a senior majoring in Computer Science with a background in quantitative development at high-frequency trading firms. I'd be keen to help with the standard array of core CS and programming-heavy Engineering courses. Join me also if you're keen on interview prep for SWE or quant roles. ",
+        image: "",
+        email: "lp2233@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/3618794377",
+    },
+    {
+        id: 6,
+        name: "Krises Maskey",
+        bio: "Hi! I am a sophomore majoring in Computer Science and minoring in economics. I am passionate about coding application systems and interested in data science. I can help you with core CS concepts and coding in Python, Java or C++.",
+        image: "images/tutors/KrisesMaskey.jpg",
+        email: "km5062@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/3618794377",
     },
 
 
