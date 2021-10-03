37bfe09b93b9cc1d47f0adfaeb79ced9560133fb
Akhat <39861196+akhatsuleimenov@users.noreply.github.com>
2021-10-03 21:28:28 +0400

    Updated Akhat's shift

--- data/shifts.js
+++ data/shifts.js
@@ -2,44 +2,44 @@ var shifts = [
     {
         id: 1,
         day: "monday",
-        from: "11:00",
-        to: "12:00"
-    },
-    {
-        id: 1,
-        day: "monday",
-        from: "16:30",
-        to: "19:00"
+        from: "19:00",
+        to: "20:00"
     },
     {
         id: 1,
         day: "tuesday",
         from: "16:30",
-        to: "19:00"
+        to: "18:00"
+    },
+    {
+        id: 1,
+        day: "tuesday",
+        from: "19:00",
+        to: "21:30"
     },
     {
         id: 1,
         day: "wednesday",
-        from: "19:30",
+        from: "19:00",
         to: "21:30"
     },
     {
         id: 1,
         day: "thursday",
-        from: "19:30",
+        from: "19:00",
         to: "21:30"
     },
     {
         id: 1,
         day: "friday",
-        from: "11:00",
-        to: "12:00"
+        from: "13:00",
+        to: "15:30"
     },
     {
         id: 1,
         day: "friday",
-        from: "14:00",
-        to: "18:00"
+        from: "19:00",
+        to: "21:30"
     },
     {
         id: 2,
