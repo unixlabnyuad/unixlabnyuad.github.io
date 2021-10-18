9b86e6d8b2f294efeff03e838b2c3f57353f4649
Krises Maskey <84721964+KrisesMaskey@users.noreply.github.com>
2021-10-19 02:57:50 +0400

    Update shifts.js

--- data/shifts.js
+++ data/shifts.js
@@ -134,8 +134,8 @@ var shifts = [
     {
         id: 6,
         day: "tuesday",
-        from: "15:00",
-        to: "16:30"
+        from: "11:00",
+        to: "12:30"
     },
     {
         id: 6,
