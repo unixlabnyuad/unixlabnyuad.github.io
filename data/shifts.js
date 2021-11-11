ed0e52e9abbfb7e14177d1538a37e7f9b4089471
Lauren McMillen <laurenamcmillen@gmail.com>
2021-11-11 13:06:57 +0400

    changed laurens schedule

--- data/shifts.js
+++ data/shifts.js
@@ -193,9 +193,21 @@ var shifts = [
     },
     {
         id: 7,
-        day: "monday",
-        from: "10:00",
-        to: "11:00"
+        day: "sunday",
+        from: "13:00",
+        to: "15:00"
+    },
+    {
+        id: 7,
+        day: "sunday",
+        from: "15:30",
+        to: "19:00"
+    },
+    {
+        id: 7,
+        day: "sunday",
+        from: "21:00",
+        to: "23:00"
     },
     {
         id: 7,
@@ -205,38 +217,15 @@ var shifts = [
     },
     {
         id: 7,
-        day: "tuesday",
-        from: "10:00",
-        to: "13:00"
-    },
-    {
-        id: 7,
-        day: "wednesday",
-        from: "10:00",
-        to: "12:00"
-    },
-    {
-        id: 7,
         day: "wednesday",
         from: "14:00",
+        to: "17:30"
+    },
+    {
+        id: 7,
+        day: "thursday",
+        from: "15:00",
         to: "16:00"
     },
-    {
-        id: 7,
-        day: "wednesday",
-        from: "17:00",
-        to: "18:30"
-    },
-    {
-        id: 7,
-        day: "thursday",
-        from: "10:00",
-        to: "11:00"
-    },
-    {
-        id: 7,
-        day: "thursday",
-        from: "12:00",
-        to: "13:00"
-    },
 ];
+
