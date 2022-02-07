bd9d380293a74ffe1ee3cf6d2895a51a22b2f92e
Akhat <39861196+akhatsuleimenov@users.noreply.github.com>
2022-02-07 19:26:06 +0400

    Created shifts.js

---
+++ data/shifts.js
@@ -0,0 +1,85 @@
+var shifts = [
+  /*  1: Akhat
+      2: Lachlan
+      3: Khola
+      4: Krises
+      5: Shehryar
+      6: Ayesha
+      7: Omar
+      8: Mathilde
+      9: Sashank
+      10: Hasin
+      11: Pedro
+      12: Saga
+      13: Himanshi
+      14: Ngoc
+      15: Desmond
+  */
+    {
+        id: 1,
+        day: "monday",
+        from: "10:00",
+        to: "11:30"
+    },
+    {
+        id: 1,
+        day: "monday",
+        from: "17:30",
+        to: "19:00"
+    },
+    {
+        id: 1,
+        day: "monday",
+        from: "20:00",
+        to: "21:30"
+    },
+    {
+        id: 1,
+        day: "tuesday",
+        from: "10:30",
+        to: "11:30"
+    },
+    {
+        id: 1,
+        day: "tuesday",
+        from: "20:30",
+        to: "21:30"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "10:00",
+        to: "11:30"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "17:30",
+        to: "19:00"
+    },
+    {
+        id: 1,
+        day: "wednesday",
+        from: "20:00",
+        to: "21:30"
+    },
+      {
+        id: 1,
+        day: "thursday",
+        from: "10:30",
+        to: "11:30"
+    },
+    {
+        id: 1,
+        day: "thursday",
+        from: "20:30",
+        to: "21:30"
+    },
+    {
+        id: 1,
+        day: "friday",
+        from: "13:00",
+        to: "15:00"
+    }
+  ]
+    
