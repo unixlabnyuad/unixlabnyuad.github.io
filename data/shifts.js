2926e4c3091d740e52acb363474e39596f0b596e
Lachlan Pham <44896700+lache-moi@users.noreply.github.com>
2022-02-07 22:29:53 +0400

    initial shifts (tutor ids 1-8)
    Spring 2022

--- data/shifts.js
+++ data/shifts.js
@@ -16,6 +16,7 @@
   */
 
 var shifts = [
+    // Akhat
     {
         id: 1,
         day: "monday",
@@ -81,6 +82,229 @@ var shifts = [
         day: "friday",
         from: "13:00",
         to: "15:00"
-    }
+    },
+
+    // Lachlan
+    {
+        id: 2,
+        day: "monday",
+        from: "11:00",
+        to: "13:30"
+    },
+    {
+        id: 2,
+        day: "tuesday",
+        from: "12:30",
+        to: "14:30"
+    },
+    {
+        id: 2,
+        day: "wednesday",
+        from: "11:30",
+        to: "13:30"
+    },
+    {
+        id: 2,
+        day: "thursday",
+        from: "11:30",
+        to: "14:30"
+    },
+    {
+        id: 3,
+        day: "thursday",
+        from: "11:30",
+        to: "14:30"
+    },
+
+    // Khola
+    {
+        id: 3,
+        day: "thursday",
+        from: "11:30",
+        to: "24:00"
+    },
+    {
+        id: 3,
+        day: "friday",
+        from: "0:00",
+        to: "2:00"
+    },
+    {
+        id: 3,
+        day: "friday",
+        from: "22:00",
+        to: "24:00"
+    },
+    {
+        id: 3,
+        day: "saturday",
+        from: "0:00",
+        to: "1:30"
+    },
+    {
+        id: 3,
+        day: "saturday",
+        from: "22:00",
+        to: "24:00"
+    },
+    {
+        id: 3,
+        day: "sunday",
+        from: "0:00",
+        to: "1:30"
+    },
+
+    // Krises
+    {
+        id: 4,
+        day: "sunday",
+        from: "10:00",
+        to: "13:00"
+    },
+    {
+        id: 4,
+        day: "monday",
+        from: "23:00",
+        to: "24:00"
+    },
+    {
+        id: 4,
+        day: "tuesday",
+        from: "0:00",
+        to: "2:00"
+    },
+    {
+        id: 4,
+        day: "wednesday",
+        from: "23:00",
+        to: "24:00"
+    },
+    {
+        id: 4,
+        day: "thursday",
+        from: "0:00",
+        to: "2:00"
+    },
+    {
+        id: 4,
+        day: "friday",
+        from: "10:00",
+        to: "13:00"
+    },
+    {
+        id: 4,
+        day: "saturday",
+        from: "10:00",
+        to: "13:00"
+    },
+
+    // Shehryar
+    {
+        id: 5,
+        day: "sunday",
+        from: "11:30",
+        to: "13:30"
+    },
+    {
+        id: 5,
+        day: "sunday",
+        from: "21:30",
+        to: "22:30"
+    },
+    {
+        id: 5,
+        day: "monday",
+        from: "19:00",
+        to: "20:30"
+    },
+    {
+        id: 5,
+        day: "tuesday",
+        from: "21:30",
+        to: "24:00"
+    },
+    {
+        id: 5,
+        day: "wednesday",
+        from: "16:30",
+        to: "17:30"
+    },
+    {
+        id: 5,
+        day: "saturday",
+        from: "11:30",
+        to: "13:30"
+    },
+
+    // Ayesha
+    {
+        id: 6,
+        day: "tuesday",
+        from: "10:00",
+        to: "16:00"
+    },
+    {
+        id: 6,
+        day: "thursday",
+        from: "12:30",
+        to: "16:00"
+    },
+    {
+        id: 6,
+        day: "friday",
+        from: "15:00",
+        to: "16:00"
+    },
+
+    // Omar
+    {
+        id: 7,
+        day: "monday",
+        from: "13:30",
+        to: "16:00"
+    },
+    {
+        id: 7,
+        day: "wednesday",
+        from: "13:30",
+        to: "16:00"
+    },
+    {
+        id: 7,
+        day: "thursday",
+        from: "21:30",
+        to: "23:30"
+    },
+    {
+        id: 7,
+        day: "saturday",
+        from: "17:00",
+        to: "19:30"
+    },
+
+    // Mathilde
+    {
+        id: 8,
+        day: "monday",
+        from: "19:00",
+        to: "20:00"
+    },
+    {
+        id: 8,
+        day: "tuesday",
+        from: "15:00",
+        to: "18:30"
+    },
+    {
+        id: 8,
+        day: "wednesday",
+        from: "13:30",
+        to: "17:30"
+    },
+    {
+        id: 8,
+        day: "thursday",
+        from: "15:00",
+        to: "16:30"
+    },
   ]
-    
