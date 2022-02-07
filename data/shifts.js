2cd606dff2657ffa486e57a145f57b609ccffc22
Lachlan Pham <44896700+lache-moi@users.noreply.github.com>
2022-02-07 23:14:43 +0400

    initial shifts (tutor ids 1-14)
    Spring 2022

--- data/shifts.js
+++ data/shifts.js
@@ -114,7 +114,7 @@ var shifts = [
     {
         id: 3,
         day: "thursday",
-        from: "11:30",
+        from: "23:30",
         to: "26:00"
     },
     {
@@ -271,4 +271,210 @@ var shifts = [
         from: "15:00",
         to: "16:30"
     },
+
+    // Sashank
+    {
+        id: 9,
+        day: "monday",
+        from: "20:30",
+        to: "23:00"
+    },
+    {
+        id: 9,
+        day: "tuesday",
+        from: "11:30",
+        to: "12:30"
+    },
+    {
+        id: 9,
+        day: "thursday",
+        from: "21:00",
+        to: "23:00"
+    },
+    {
+        id: 9,
+        day: "friday",
+        from: "10:00",
+        to: "14:30"
+    },
+    {
+        id: 9,
+        day: "saturday",
+        from: "13:00",
+        to: "14:30"
+    },
+
+    // Hasin
+    {
+        id: 10,
+        day: "monday",
+        from: "14:00",
+        to: "17:30"
+    },
+    {
+        id: 10,
+        day: "monday",
+        from: "23:30",
+        to: "24:30"
+    },
+    {
+        id: 10,
+        day: "wednesday",
+        from: "17:30",
+        to: "20:00"
+    },
+    {
+        id: 10,
+        day: "wednesday",
+        from: "22:00",
+        to: "23:00"
+    },
+    {
+        id: 10,
+        day: "thursday",
+        from: "16:00",
+        to: "20:00"
+    },
+    {
+        id: 10,
+        day: "friday",
+        from: "19:00",
+        to: "22:00"
+    },
+
+    // Pedro
+    {
+        id: 11,
+        day: "monday",
+        from: "15:30",
+        to: "19:00"
+    },
+    {
+        id: 11,
+        day: "wednesday",
+        from: "20:30",
+        to: "22:00"
+    },
+    {
+        id: 11,
+        day: "friday",
+        from: "14:30",
+        to: "19:00"
+    },
+    {
+        id: 11,
+        day: "saturday",
+        from: "14:30",
+        to: "18:30"
+    },
+
+
+    // Sagynbek
+    {
+        id: 12,
+        day: "sunday",
+        from: "10:00",
+        to: "11:30"
+    },
+    {
+        id: 12,
+        day: "sunday",
+        from: "13:30",
+        to: "15:00"
+    },
+    {
+        id: 12,
+        day: "monday",
+        from: "21:30",
+        to: "23:30"
+    },
+    {
+        id: 12,
+        day: "tuesday",
+        from: "16:30",
+        to: "20:30"
+    },
+    {
+        id: 12,
+        day: "wednesday",
+        from: "10:00",
+        to: "11:30"
+    },
+    {
+        id: 12,
+        day: "wednesday",
+        from: "22:00",
+        to: "23:30"
+    },
+    {
+        id: 12,
+        day: "thursday",
+        from: "16:30",
+        to: "17:30"
+    },
+    {
+        id: 12,
+        day: "friday",
+        from: "22:00",
+        to: "24:00"
+    },
+
+    // Himanshi
+    {
+        id: 13,
+        day: "sunday",
+        from: "15:00",
+        to: "17:00"
+    },
+    {
+        id: 13,
+        day: "monday",
+        from: "10:00",
+        to: "11:00"
+    },
+    {
+        id: 13,
+        day: "wednesday",
+        from: "23:30",
+        to: "24:30"
+    },
+    {
+        id: 13,
+        day: "thursday",
+        from: "10:30",
+        to: "12:30"
+    },
+    {
+        id: 13,
+        day: "saturday",
+        from: "22:00",
+        to: "25:00"
+    },
+
+    // Ngoc
+    {
+        id: 14,
+        day: "sunday",
+        from: "14:00",
+        to: "17:00"
+    },
+    {
+        id: 14,
+        day: "thursday",
+        from: "19:00",
+        to: "21:00"
+    },
+    {
+        id: 14,
+        day: "friday",
+        from: "16:00",
+        to: "19:00"
+    },
+    {
+        id: 14,
+        day: "saturday",
+        from: "14:00",
+        to: "17:00"
+    },
+
   ]
--- data/tutors.js
+++ data/tutors.js
@@ -22,7 +22,7 @@ var tutors =  [
         bio: "About me ... I'm happy to help you with ...",
         image: "images/tutors/waji.jpg",
         email: "@nyu.edu",
-        zoom: "",
+        //zoom: "",
       },
   */
     {
@@ -31,7 +31,7 @@ var tutors =  [
         bio: "Hi, I am a Sophomore majoring in CS. Incoming Meta Software Enginner. Alumni of 42 Software Engineering School in Silicon Valley. I'm happy to help you with any Algorithmic problem, Python, C/C++, CSO, OS, ML, SE, Intro to CS, Algorithms, Data Structures, Discrete Math, and any Math course. Also down to just talk about coding, internships, and etc.",
         image: "images/tutors/Akhat.jpg",
         email: "akhat.suleimenov@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/6171613743?pwd=Y0lSakR5Z3pUSjB1LzJnZkcrMWJIQT09",
+        //zoom: "https://nyu.zoom.us/j/6171613743?pwd=Y0lSakR5Z3pUSjB1LzJnZkcrMWJIQT09",
     },
     {
         id: 2,
@@ -39,7 +39,7 @@ var tutors =  [
         bio: "I'm a senior majoring in Computer Science with a background in quantitative development at high-frequency trading firms. I'm happy to help with the standard array of core CS and programming-heavy Engineering courses. Join me also if you're keen on interview prep for SWE or quant roles. ",
         image: "images/tutors/lachlan.jpeg",
         email: "lp2233@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/3618794377",
+        //zoom: "https://nyu.zoom.us/j/3618794377",
     },
     {
         id: 3,
@@ -47,7 +47,7 @@ var tutors =  [
         bio: "Hi, I'm a senior majoring in Math and minoring in Computer Science. I would love to help you develop your skills in python and get a better understanding of the mathematical side of CS!",
         image: "images/tutors/Khola_RAintro.jpg",
         email: "kj1263@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/97401480668",
+        //zoom: "https://nyu.zoom.us/j/97401480668",
     },
     {
         id: 4,
@@ -55,7 +55,7 @@ var tutors =  [
         bio: "Hi! I am a sophomore majoring in Computer Science and minoring in economics. I am passionate about coding application systems and interested in data science. I can help you with core CS concepts and coding in Python, Java or C++.",
         image: "images/tutors/KrisesMaskey.jpg",
         email: "km5062@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/3392538775",
+        //zoom: "https://nyu.zoom.us/j/3392538775",
     },
     {
         id: 5,
@@ -63,7 +63,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 6,
@@ -71,7 +70,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 7,
@@ -79,7 +77,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 8,
@@ -87,7 +84,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 9,
@@ -95,7 +91,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 10,
@@ -103,7 +98,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 11,
@@ -111,7 +105,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 12,
@@ -119,7 +112,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 13,
@@ -127,7 +119,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 14,
@@ -135,7 +126,6 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
     {
         id: 15,
@@ -143,6 +133,5 @@ var tutors =  [
         bio: "Coming soon!",
         image: "images/tutors/blank.png",
         email: "@nyu.edu",
-        zoom: "https://nyu.zoom.us/j/999999999",
     },
 ];
