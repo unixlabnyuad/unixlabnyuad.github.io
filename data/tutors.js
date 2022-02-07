2d6da650abf350c34dedb2975ad7ad3a4877bf29
Akhat <39861196+akhatsuleimenov@users.noreply.github.com>
2022-02-07 19:30:50 +0400

    Created tutors.js

---
+++ data/tutors.js
@@ -0,0 +1,60 @@
+var tutors =  [
+  /*  {
+        id: 1,
+        name: "Aw Samtut Or",
+        bio: "About me ... I'm happy to help you with ...",
+        image: "images/tutors/waji.jpg",
+        email: "@nyu.edu",
+        zoom: "",
+      },
+  */
+  
+  /*  1: Akhat
+      2: Lachlan
+      3: Khola
+      4: Krises
+      5: Shehryar
+      6: Ayesha
+      7: Omar
+      8: Mathilde
+      9: Sashank
+      10: Hasin
+      11: Pedro
+      12: Saga
+      13: Himanshi
+      14: Ngoc
+      15: Desmond
+  */
+    {
+        id: 1,
+        name: "Akhat Suleimenov",
+        bio: "Hi, I am a Sophomore majoring in CS. Incoming Meta Software Enginner. Alumni of 42 Software Engineering School in Silicon Valley. I'm happy to help you with any Algorithmic problem, Python, C/C++, CSO, OS, ML, SE, Intro to CS, Algorithms, Data Structures, Discrete Math, and any Math course. Also down to just talk about coding, internships, and etc.",
+        image: "images/tutors/Akhat.jpg",
+        email: "akhat.suleimenov@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/6171613743?pwd=Y0lSakR5Z3pUSjB1LzJnZkcrMWJIQT09",
+    },
+    {
+        id: 2,
+        name: "Lachlan Pham",
+        bio: "I'm a senior majoring in Computer Science with a background in quantitative development at high-frequency trading firms. I'm happy to help with the standard array of core CS and programming-heavy Engineering courses. Join me also if you're keen on interview prep for SWE or quant roles. ",
+        image: "images/tutors/lachlan.jpeg",
+        email: "lp2233@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/3618794377",
+    },
+    {
+        id: 3,
+        name: "Khola Jamshad",
+        bio: "Hi, I'm a senior majoring in Math and minoring in Computer Science. I would love to help you develop your skills in python and get a better understanding of the mathematical side of CS!",
+        image: "images/tutors/Khola_RAintro.jpg",
+        email: "kj1263@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/97401480668",
+    },
+    {
+        id: 4,
+        name: "Krises Maskey",
+        bio: "Hi! I am a sophomore majoring in Computer Science and minoring in economics. I am passionate about coding application systems and interested in data science. I can help you with core CS concepts and coding in Python, Java or C++.",
+        image: "images/tutors/KrisesMaskey.jpg",
+        email: "km5062@nyu.edu",
+        zoom: "https://nyu.zoom.us/j/3392538775",
+    },
+];
